29bcbb58ff5bc704c077d60e99e501f7fb1ee773
Gustavo Toledo <toledo970501@gmail.com>
2024-05-15 00:53:12 -0500

    Se agregan pequeño tesst unitario

--- backend/src/test/java/com/project/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/project/backend/BackendApplicationTests.java
@@ -1,27 +1,30 @@
 package com.project.backend;
 
-import com.project.backend.application.dto.PersonaResponse;
-import com.project.backend.application.handler.IPersonaHandler;
+
+import com.project.backend.domain.usecase.UseCaseUtils;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
-@DataJpaTest
 class BackendApplicationTests {
 
 
-	private final IPersonaHandler personaHandler;
-
-    BackendApplicationTests(IPersonaHandler personaHandler) {
-        this.personaHandler = personaHandler;
-    }
-
-
-    @Test
-	void contextLoads() {
-		PersonaResponse personaResp = personaHandler.getModelFrom(2L);
-		assert personaResp.getCedula().equals("1075399999");
+	@ParameterizedTest
+	@ValueSource(strings = { "HOLA", "MuNDo", "pRueBA" })
+	void testNormalizarTextNotNull(String input) {
+		String textoNormalizado = UseCaseUtils.normalizarTexto(input);
+		Assert.notNull(textoNormalizado,"Texto not null");
+		Assert.isTrue(
+				textoNormalizado.equals("Hola") ||
+						textoNormalizado.equals("Mundo") ||
+						textoNormalizado.equals("Prueba")
+				,"Texto normalizado");
 	}
 
 }
